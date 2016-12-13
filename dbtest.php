23051ef4d5c3d42169c438692190739180d178af
slava <bezgachev@gmail.com>
2016-12-13 11:25:09 +0000

    Initial draft, seems to work. Now gonna create one for silverstripe

---
+++ dbtest.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: slava
+ * Date: 12/12/16
+ * Time: 14:03
+ */
+
+mysqli_connect("db", "root", "root") or die(mysqli_error());
+echo "Connected to MySQL<br />";
